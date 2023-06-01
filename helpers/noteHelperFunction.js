be5e2ca04c5ccae430176e8cbde3ca030779f438
Albikras <124819029+Albikras@users.noreply.github.com>
2023-06-01 16:24:46 -0400

    added 2 new javascript files to deal with the paths, and deleted the not needed javascript files, also created functionality and routes for the website

---
+++ helpers/noteHelperFunction.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const util = require("util");
+
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err
+      ? console.error("Error!!")
+      : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parseNotes = JSON.parse(data);
+      parseNotes.push(content);
+      writeToFile(file, parseNotes);
+    }
+  });
+};
+
+module.exports = { readAndAppend, readFromFile };
---
+++ routes/apiRoutes.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+
+const {
+  readAndAppend,
+  readFromFile,
+} = require("../helpers/noteHelperFunction");
+
+router.get("/notes", (req, res) => {
+  console.info(`${req.method} request recieved for notes`);
+  readFromFile("./db/db.json").then((data) => {
+    res.json(JSON.parse(data));
+  });
+});
+
+router.post("/notes", (req, res) => {
+  console.info(`${req.method} request recieved for notes`);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNotes = {
+      title,
+      text,
+    };
+
+    readAndAppend(newNotes, "./db/db.json");
+    res.json("notes added successfully");
+  } else {
+    res.error("Error!!");
+  }
+});
+module.exports = router;
